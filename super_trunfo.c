b54b6e67c7894fd61e177c9e1fcc5cc40e2f5b75
CapuccinoThatWolf <wflufy@gmail.com>
2025-05-06 20:41:42 -0300

    Update super_trunfo.c
    desafio nível aventureiro concluído

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,6 +9,11 @@ int main() {
     float area1;
     float pib1;
     int pontos_turisticos1;
+    //Densidade populacional e PIB Per Capta da primeira carta
+    float densidadepop1;
+    float pibpercapta1;
+    densidadepop1 = populacao1 / area1;
+    pibpercapta1 = (pib1 * 1000000000) / populacao1;
 
     // Variáveis para a segunda carta
     char estado2;
@@ -18,6 +23,11 @@ int main() {
     float area2;
     float pib2;
     int pontos_turisticos2;
+    //Densidade populacional e PIB Per Capta da segunda carta
+    float densidadepop2;
+    float pibpercapta2;
+    densidadepop2 = populacao2 / area2;
+    pibpercapta2 = (pib2 * 1000000000) / populacao2;
 
     // Leitura dos dados da primeira carta
     printf("Digite os dados da primeira carta:\n");
@@ -76,6 +86,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepop1);
+    printf("PIB Per Capita: %.2f reais\n", pibpercapta1);
 
     // Exibição dos dados da segunda carta
     printf("\nCarta 2:\n");
@@ -86,6 +98,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepop2);
+    printf("PIB Per Capita: %.2f reais\n", pibpercapta2);
 
     return 0;
 }
